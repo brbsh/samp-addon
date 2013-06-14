73fb0279be3e34c99d02f5c7f5fe6b32570dc07f
Vladislav Barabash <bjiadokc@gmail.com>
2013-06-14 09:38:31 +0600

    Fixed screenshooting bug

--- screen.cpp
+++ screen.cpp
@@ -10,16 +10,18 @@ void addonScreen::Get(std::string filename)
 {
 	DWORD addr = 0x5D0820; // screenshot take addr
 	char *c_filename = new char[(filename.length() + 1)];
+	
+	strcpy(c_filename, filename.c_str())
 
 	_asm
 	{
-		mov		eax, [0xB6F97C]
-		mov		eax, [eax]
+		mov	eax, [0xB6F97C]
+		mov	eax, [eax]
 		push	c_filename
 		push	eax
 		call	addr
-		add		esp, 8
+		add	esp, 8
 	}
 
-	delete c_filename;
+	delete[] c_filename;
 }
